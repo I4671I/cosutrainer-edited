0612b74ac4bd0e24fdee60ef8a975e88b12f2f63
syj <syj24@mails.tsinghua.edu.cn>
2026-06-13 19:15:13 +0800

    change diff format for self use

--- src/mapeditor.h
+++ src/mapeditor.h
@@ -122,7 +122,7 @@ struct editpass // temporary data that's only needed in editing (only passed wit
     float orig_ar, orig_od;
 };
 
-#define DEFAULT_FMT "@diffname@ @RATE@ @BPM@ @emuldt@ @cs@ @ar@ @od@ @hp@ @cut@ @flip@ @nosv@"
+#define DEFAULT_FMT "@diffname@ @rate@ @(bpm)@ @emuldt@ @cs@ @ar@ @od@ @hp@ @cut@ @flip@ @nosv@"
 
 #define CUSTOM_DIFF_VAR "COSU_CUSTOM_DIFF_FORMAT"
 
